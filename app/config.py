93fdf018f69bc21fe5fa5d698ea4b4fb1c3cff00
Christian E. Velasquez <christian.velasquez.1220@gmail.com>
2025-06-14 00:04:47 +0800

    Use DATABASE_URL from environment for Render deployment

--- app/config.py
+++ app/config.py
@@ -10,10 +10,10 @@ db_credentials = {
 }
 
 # mqtt credentials
-mqtt_credentials = {
-  'mqtt_broker': 'localhost',
-  'mqtt_port': 1883 # Default MQTT port
-}
+# mqtt_credentials = {
+#   'mqtt_broker': 'localhost',
+#   'mqtt_port': 1883 # Default MQTT port
+# }
 
 # secret key
 secret_key = 'ashley-macasadia_luisa-gonzales_christian-velasquez_kier-pobre_chaira-catibog_'
--- app/database/session.py
+++ app/database/session.py
@@ -1,9 +1,17 @@
+import os
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 from app.config import db_credentials
 
-db_url = f'mysql+pymysql://{db_credentials['db_user']}@{db_credentials['db_host']}/{db_credentials['db_name']}'
+# db_url = f'mysql+pymysql://{db_credentials['db_user']}@{db_credentials['db_host']}/{db_credentials['db_name']}'
+# engine = create_engine(db_url)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+db_url = os.getenv('DATABASE_URL')
+if db_url and db_url.startswith('mysql://'):
+  db_url = db_url.replace('mysql://', 'mysql+pymysql://', 1)
+
 engine = create_engine(db_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
