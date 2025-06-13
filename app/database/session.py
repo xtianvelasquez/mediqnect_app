82de111cb484d80246c252662194a813cbabbece
Christian E. Velasquez <christian.velasquez.1220@gmail.com>
2025-06-14 01:02:32 +0800

    database debuggin

--- app/database/session.py
+++ app/database/session.py
@@ -3,11 +3,15 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
-load_dotenv()
+if os.getenv('RENDER') is None:
+  load_dotenv()
 
 db_url = os.getenv('DATABASE_URL')
 if db_url and db_url.startswith('mysql://'):
   db_url = db_url.replace('mysql://', 'mysql+pymysql://', 1)
+
+if not db_url:
+  raise ValueError('DATABASE_URL is not set.')
 
 engine = create_engine(db_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
